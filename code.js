b0771dd0fb0bfb3e3a4fa06cf1547f36adebc088
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-03-03 13:43:08 -0600

    Create code.js

---
+++ code.js
@@ -0,0 +1,2 @@
+function alex() {
+  javascript:var imgs = document.getElementsByTagName("img");for(var i=0, l=imgs.length;i<l;i++){imgs[i].src = "https://github.com/Aerolixdev/ImageDUMP/blob/main/images/Alex.png?raw=true";}
