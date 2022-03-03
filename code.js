341da752036b2cc2886318af3e2c41d898e50078
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-03-03 15:34:25 -0600

    Delete code.js

--- code.js
+++
@@ -1,2 +0,0 @@
-function alex() {
-  var imgs = document.getElementsByTagName("img");for(var i=0, l=imgs.length;i<l;i++){imgs[i].src = "https://github.com/Aerolixdev/ImageDUMP/blob/main/images/Alex.png?raw=true";}
